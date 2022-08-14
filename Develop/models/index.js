ef73e46b26a680497f0f040c9739063b99937ce5
Chris Newbold <104353732+ChrisNewbold@users.noreply.github.com>
2022-08-14 22:52:51 +1000

    update routes and models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags_product'
+});
 module.exports = {
   Product,
   Category,
