c91337597fa30855bc77ae6f4244195e9d89fe81
Chris Newbold <104353732+ChrisNewbold@users.noreply.github.com>
2022-08-15 23:51:04 +1000

    change DELETE Tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -58,6 +58,7 @@ router.put('/:id', async (req, res) => {
 // delete on tag by its `id` value
 router.delete('/:id', async (req, res) => {
   try {
+    console.log(req.params)
     const tagData = await Tag.destroy({
       where: {
         id: req.params.id,
@@ -67,7 +68,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: "No tag found with that id!" });
       return;
     }
-    res.status(200).json(err);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
